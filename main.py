e74964bf287fd32b32eee121f8904e54ed6d36cf
Mohamed Hasabalah <68168970+mhasabalah@users.noreply.github.com>
2022-01-02 01:10:35 +0200

    MySql confg

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from Health_Asurance import create_app
+from flask_mysqldb import MySQL
+
 
 app = create_app()
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'MyDB'
 
-
+mysql = MySQL(app)
 
 if __name__ == '__main__':
     app.run(debug=True) 
