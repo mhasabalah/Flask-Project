97b64980ba7f1f574204f1ed8fbbf53da3c8faa6
Mohamed Hasabalah <68168970+mhasabalah@users.noreply.github.com>
2022-01-05 19:41:13 +0200

    For modal

--- Health_Asurance/static/js/mainBase.js
+++ Health_Asurance/static/js/mainBase.js
@@ -1,8 +1,8 @@
-// $(document).ready(function () {
-//         $('#myModal').on('shown.bs.modal', function () {
-//             $('#myInput').trigger('focus')
-//             })
-// });
+$(document).ready(function () {
+        $('#myModal').on('shown.bs.modal', function () {
+            $('#myInput').trigger('focus')
+            })
+});
 
 
 
